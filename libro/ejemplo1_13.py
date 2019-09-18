80f056d8393c44bdb094cfbcf0826f7aaadc5821
augus2349 <54091892+augus2349@users.noreply.github.com>
2019-09-17 22:45:13 -0500

    se añadio un nuevo ejemplo

---
+++ libro/ejemplo1_13.py
@@ -0,0 +1,8 @@
+MAT = 5
+CAL1 = float(input("ingrese la primer calificacion: "))
+CAL2 = float(input("ingrese la segunda calificacion: "))
+CAL3 = float(input("ingrese la tercer calificacion: "))
+CAL4 = float(input("ingrese la cuarta calificacion: "))
+CAL5 = float(input("ingrese la quinta calificacion: "))
+PRO = (CAL1 + CAL2 + CAL3 + CAL4 + CAL5) / MAT
+print(f"el promedio de las { MAT } materias es de: { PRO }")
