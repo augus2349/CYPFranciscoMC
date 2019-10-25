55ab878fe4d76192fa9c8ce3978243910a544cea
augus2349 <54091892+augus2349@users.noreply.github.com>
2019-10-24 22:08:18 -0500

    tarea

---
+++ libro/problemas_resueltos/capitulo3/problema3_6.py
@@ -0,0 +1,10 @@
+N = int(input("ingrese el numero de datos a ingresar "))
+MAY = -100000
+MEN = 100000
+for i in range(0,N,1):
+    NUM int(input("ingrese un numero entero "))
+    if NUM > MAY:
+        MAY = NUM
+    if NUM < MEN:
+        MEN = NUM
+print(f"el maximo es {MAY} y el minimo es {MEN}")
